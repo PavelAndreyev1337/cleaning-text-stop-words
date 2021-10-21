0f69a566b88b98c4ed267f4125f6c05bcaeaeb79
PavelAndreyev1337 <pavel.andreyev.98@gmail.com>
2021-10-21 19:21:56 +0300

    added calculation of words frequency

--- application.py
+++ application.py
@@ -1,9 +1,19 @@
+from typing import Dict
 from text_processing.text_cleaner import TextCleaner
 
 
 class Application:
     def __init__(self):
         self.__text_cleaner = TextCleaner()
+
+    def __print_paragraphs(self, paragraphs: Dict[int, int]):
+        for paragraph_number, count in paragraphs.items():
+                print("Paragraph: ", paragraph_number + 1, "\n", count)
+
+    def __print_paragraphs_words_frequency(self, paragraph_words_frequency: Dict[str, Dict[int, int]]):
+        for word, paragraphs in paragraph_words_frequency.items():
+            print("Word: ", word)
+            self.__print_paragraphs(paragraphs)
 
     def run(self):
         self.__text_cleaner.clear()
@@ -15,3 +25,12 @@ class Application:
         print("\nPercentage of stop words in the given text:",
               str(int(round(self.__text_cleaner
                             .get_last_stop_words_percentage(), 2) * 100)) + "%")
+        self.__text_cleaner.export_last_relative_frequency_to_csv()
+        print("\nFrequent words:", self.__text_cleaner.frequent_words.keys())
+        print("\nFrequency of words in paragraphs:")
+        self.__print_paragraphs_words_frequency(self.__text_cleaner.paragraph_words_frequency)
+        print("\nNumber of words per paragraph:")
+        self.__print_paragraphs(self.__text_cleaner.paragraph_words_counts)
+        print("\nWords count:", self.__text_cleaner.get_words_count())
+        print("\n Relative_frequency:")
+        self.__print_paragraphs_words_frequency(self.__text_cleaner.relative_frequency)
--- text_processing/text_cleaner.py
+++ text_processing/text_cleaner.py
@@ -2,7 +2,9 @@ from nltk import download
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from docx import Document
-from typing import List, Set
+from typing import Dict, List, Set
+from openpyxl import Workbook
+from collections import Counter
 
 
 class TextCleaner:
@@ -16,6 +18,18 @@ class TextCleaner:
         self.__file_path = file_path
         self.__output_file_path = output_file_path
         self.__document = Document(self.__file_path)
+        self.__workbook = None
+        # used pr-cy.ru {key:word : value:the root of the word, }
+        self.__frequent_words = []
+        # the frequency of words in the text {key: word, value: count,}
+        self.__text_words_frequency = {}
+        # frequency of words in paragraphs {key: word, value: {key:paragraph_number: value: count,},}
+        self.__paragraph_words_frequency = {}
+        # the number of words in a paragraph {key: paragraph_number, value: count}
+        self.__paragraph_words_counts = {}
+        self.__frequent_words_count = 0
+        self.__relative_frequency = {}  # 3-digit accuracy after coma
+        self.__output_xlsx_file_path = "output.xlsx"
         self.__last_text_words_count = 0
         self.__last_used_stop_words = []
 
@@ -49,6 +63,29 @@ class TextCleaner:
     def last_used_stop_words(self) -> List[str]:
         return self.__last_used_stop_words
 
+    @property
+    def frequent_words(self):
+        return self.__frequent_words
+
+    @property
+    def text_words_frequency(self):
+        return self.__text_words_frequency
+
+    @property
+    def paragraph_words_frequency(self):
+        return self.__paragraph_words_frequency
+
+    @property
+    def paragraph_words_counts(self):
+        return self.__paragraph_words_counts
+
+    @property
+    def relative_frequency(self):
+        return self.__relative_frequency
+
+    def get_words_count(self):
+        return sum([words_count for words_count in self.__paragraph_words_counts.values()])
+
     def get_last_stop_words_percentage(self) -> float:
         return len(self.__last_used_stop_words) / self.__last_text_words_count
 
@@ -74,6 +111,78 @@ class TextCleaner:
     def __reset_results(self):
         self.__last_text_words_count = 0
         self.__last_used_stop_words = []
+
+    def __calculate_words_count(self):
+        self.__document = Document(self.__output_file_path)
+        for key_word, root in self.__frequent_words.items():
+            self.__text_words_frequency[key_word] = 0
+            self.__paragraph_words_frequency[key_word] = {}
+            for i, paragraph in enumerate(self.__document.paragraphs):
+                self.__paragraph_words_frequency[key_word][i] = 0
+                self.__paragraph_words_counts[i] = 0
+                for run in paragraph.runs:
+                    words = word_tokenize(run.text)
+                    self.__paragraph_words_counts[i] += len(words)
+                    for word in words:
+                        if word.startswith(root):
+                            self.__text_words_frequency[key_word] += 1
+                            self.__paragraph_words_frequency[key_word][i] += 1
+        self.__workbook.save(self.__output_xlsx_file_path)
+        self.__text_words_frequency = {word: count for word, count in
+                                       sorted(self.__text_words_frequency.items(),
+                                              key=lambda item: item[1],
+                                              reverse=True)}
+
+    def __calculate_relative_frequency(self):
+        for word, paragraphs in self.__paragraph_words_frequency.items():
+            self.__relative_frequency[word] = {}
+            for paragraph_number, words_count in paragraphs.items():
+                paragraph_words_count = self.__paragraph_words_counts[paragraph_number]
+                if not paragraph_words_count:
+                    self.__relative_frequency[word][paragraph_number] = 0
+                else:
+                    self.__relative_frequency[word][paragraph_number] = round(
+                        words_count / self.__paragraph_words_counts[paragraph_number], 3)
+
+    def export_last_relative_frequency_to_csv(self,
+                                              frequent_words: Dict[str, str] = {},
+                                              output_xlsx_file_path: str = "Андреєв_етап3.xlsx",
+                                              frequent_words_count: int = 21):  # stage 3
+        self.__output_xlsx_file_path = output_xlsx_file_path
+        self.__frequent_words_count = frequent_words_count
+        self.__workbook = Workbook()
+        if not frequent_words:
+            self.__frequent_words = {  # result from pr-cy.ru
+                "автосамосвал": "автосамосвал",
+                "модель": "модел",
+                "транспортный": "транспортн",
+                "движение": "движен",
+                "карьер": "карьер",
+
+                "работа": "работ",
+                "состояние": "состоян",
+                "разгрузка": "разгрузк",
+                "блок": "блок",
+                "пункт": "пункт",
+
+                "погрузка": "погрузк",
+                "система": "систем",
+                "экскаватор": "экскаватор",
+                "время": "врем",
+                "управление": "управлен",
+
+                "имитационный": "имитационн",
+                "параметр": "параметр",
+                "временить": "времен",
+                "скорость": "скорост",
+                "цикл": "цикл",
+
+                "граф": "граф",
+            }
+        else:
+            self.__frequent_words = frequent_words
+        self.__calculate_words_count()
+        self.__calculate_relative_frequency()
 
     def clear(self):
         self.__reset_results()
